8f286e62a122c28a8da66551f97b21fe8f03a724
Emilio Schepis <emilio.schepis@gmail.com>
2021-07-31 19:02:38 +0200

    feat: handle anonymous users

--- functions/src/handlers/onCreateUserHandler.ts
+++ functions/src/handlers/onCreateUserHandler.ts
@@ -6,15 +6,22 @@ import { sdk } from "../graphql/client";
 export default async function onCreateUserHandler(
   user: functions.auth.UserRecord
 ) {
+  const isAnonymous = user.providerData.length === 0;
+
   const customUserClaims = {
     "https://hasura.io/jwt/claims": {
-      "x-hasura-default-role": "user",
-      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-default-role": isAnonymous ? "anonymous" : "user",
+      "x-hasura-allowed-roles": isAnonymous ? ["anonymous"] : ["user"],
       "x-hasura-user-id": user.uid,
     },
   };
 
   await admin.auth().setCustomUserClaims(user.uid, customUserClaims);
+
+  if (isAnonymous) {
+    // Do not create a database record for this anonymous user.
+    return;
+  }
 
   return sdk.CreateUser({
     id: user.uid,
