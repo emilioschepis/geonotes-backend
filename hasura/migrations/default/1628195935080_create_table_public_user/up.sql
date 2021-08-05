5e0f51e73448ef2056e8061dfa0d48e376eea5c2
Emilio Schepis <emilio.schepis@gmail.com>
2021-08-05 22:56:38 +0200

    feat: rewrite metadata & migrations

---
+++ hasura/migrations/default/1628195935080_create_table_public_user/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."user" ("id" text NOT NULL, "email" text NOT NULL, "username" text NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , UNIQUE ("email"), UNIQUE ("username"));
